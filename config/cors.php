150a6f319d71b70f44c0001bc6ddbf85c0b5479b
Nawaz Sharief syed <nawaz.sharief786@gmail.com>
2025-08-25 15:18:08 +0530

    updatd allowed headers

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Accept', 'Origin', 'Authorization'],
 
     'exposed_headers' => [],
 
